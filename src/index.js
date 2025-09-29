0b3fb6368e81452e6e803e03a931db0cdf201b71
Mech <ssandhyac747@gmail.com>
2025-09-29 15:21:26 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React library to use JSX and build components
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import createRoot from react-dom/client (new in React 18+)
+// This is used to render the React app into the DOM (HTML page).
+import { createRoot } from "react-dom/client";
+
+// Import BrowserRouter from react-router-dom
+// BrowserRouter allows us to use routing (navigation between pages/components).
+import { BrowserRouter } from "react-router-dom";
+
+// Import the main App component (root component of our application)
+import App from "./App";
+
+// Get the root <div> element from index.html (public/index.html has <div id="root"></div>)
+// This is where our React app will be mounted inside the browser.
+const container = document.getElementById("root");
+
+// Create a root object using React 18's createRoot API
+// This replaces the old ReactDOM.render() method (React < 18).
+const root = createRoot(container);
+
+// Render the React application inside the root container
+// Wrapping <App /> with <BrowserRouter> enables routing features like <Route>, <Link>, etc.
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
